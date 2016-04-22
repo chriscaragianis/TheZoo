ccefeaac86bca0f0133031d3294ebd7e2fae5b8f
Chris Caragianis <chriscaragianis@gmail.com>
2016-04-22 09:10:20 -0400

    Add primitive testing

---
+++ DogTest.java
@@ -0,0 +1,48 @@
+public class DogTest extends Test {
+    
+    public Dog testDog;
+    
+    public DogTest() {
+        super("Dog");
+        testDog = new Dog("Fido");
+    }
+    
+    public int[] runTests() {
+        int[] result = new int[3];
+        result[0] = 2;
+        //testEat
+        int indicator = testEat();
+        if (indicator == 0) {
+          result[1]++;
+        }
+        if (indicator == -1) {
+          result[2]++;
+        }
+        //testStuntList
+        indicator = testStuntList();
+        if (indicator == 0) {
+          result[1]++;
+        }
+        if (indicator == -1) {
+          result[2]++;
+        }
+        return result;
+    }
+    
+    //tests
+    private int testEat() {
+        String out = testDog.eat();
+        if (out.equals("Fido wolfs down kibble")) {
+            System.out.println("Dog#eat: 👍");
+            return 0;
+        }
+        else {
+            System.out.println("Dog#eat:  💩  expected \"" + out + 
+                               "\" to equal \"Fido wolfs down kibble");
+            return -1;
+        }
+    }
+    private int testStuntList() {return 0;}
+    
+    
+}
---
+++ Test.java
@@ -0,0 +1,30 @@
+public abstract class Test {
+    
+    private String className;
+    
+    public String getClassName() {return className;}
+    
+    public Test(String cname) {
+        className = cname;
+    }
+    
+    public abstract int[] runTests(); 
+    //each test method in a Test class should
+    //print a message and return 0 (pass) or
+    //-1 (fail)
+    //
+    //runTests should return [# of tests, # of passes, # of failures]
+    public void displayResults() {
+        int[] results = runTests();
+        System.out.println("Tests for " + className + "\n");
+        if (results[2] != 0) {
+            System.out.println("There were failures!");
+        }
+        else {
+          System.out.println("Success");
+        }
+        System.out.println("Examples: " + results[0] + 
+                           ", Passed: " + results[1] + 
+                           ", Failed: " + results[2]);
+    }
+}
